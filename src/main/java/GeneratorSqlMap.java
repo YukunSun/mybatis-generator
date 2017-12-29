dd6ba28d7c5225626498b3673e945d2d2d7059a9
sunyk <sunyukun@shopin.cn>
2017-12-29 15:30:37 +0800

    init

---
+++ src/main/java/GeneratorSqlMap.java
@@ -0,0 +1,39 @@
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author: sun.yukun@foxmail.com
+ * Time: 2017/12/29 下午2:06
+ * Blog: coderdaily.net
+ */
+public class GeneratorSqlMap {
+    public void generator() throws Exception {
+
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        //指定 逆向工程配置文件
+        File configFile = new File("src/main/resources/generatorConfig.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config,
+                callback, warnings);
+        myBatisGenerator.generate(null);
+    }
+
+    public static void main(String[] args) throws Exception {
+        try {
+            GeneratorSqlMap generatorSqlmap = new GeneratorSqlMap();
+            generatorSqlmap.generator();
+            System.out.println("Success!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
